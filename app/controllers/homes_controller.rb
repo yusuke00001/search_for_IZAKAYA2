a53a1ec838b5a995534bf65189cd0f4060aee04c
yusuke00001 <volleyball.pipi@icloud.com>
2025-02-03 13:04:57 +0900

    homes_pathのアクセス制限の実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,16 @@
 class HomesController < ApplicationController
+  before_action :current_user?
   def index
   end
 
   def create
     current_user.avatar.attach(params[:user][:avatar])
+  end
+
+  def current_user?
+    unless current_user.present?
+      redirect_to user_session_path
+      flash[:alert] = "ログインしてください"
+    end
   end
 end
