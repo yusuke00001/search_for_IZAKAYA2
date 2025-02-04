e3e85e602242f8e815bdc8cfbdbc98a95efd1b8b
ゆうすけ <yuusuke@naitouyukenoAir.lan>
2025-02-04 12:20:36 +0900

    authenticate_userメソッドの使用

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,16 +1,9 @@
 class HomesController < ApplicationController
-  before_action :current_user?
+  before_action :authenticate_user!
   def index
   end
 
   def create
     current_user.avatar.attach(params[:user][:avatar])
-  end
-
-  def current_user?
-    unless current_user.present?
-      redirect_to user_session_path
-      flash[:alert] = "ログインしてください"
-    end
   end
 end
