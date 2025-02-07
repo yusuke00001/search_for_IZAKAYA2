03c3e5d1859bd2a6f1ed6e13ef9c03db52022ba9
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-02-07 12:24:59 +0900

    店舗情報の表示

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,4 @@
 class ShopsController < ApplicationController
-  require "open-uri"
   def index
     keyword = params[:keyword] || "名古屋市"
     @shops = HotpepperAPI.search_shops(keyword)
--- app/services/hotpepper_api.rb
+++ app/services/hotpepper_api.rb
@@ -4,7 +4,7 @@ require "json"
 class HotpepperAPI
   BASE_URL = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
 
-  def self.search_shops(keyword, count = 10)
+  def self.search_shops(keyword, count = 10) # これでAPIにリクエストを送信
     response = Faraday.get(BASE_URL, {
       key: ENV["HOTPEPPER_API_KEY"],
       keyword: keyword, # 検索に使うキーワード
@@ -12,7 +12,7 @@ class HotpepperAPI
       count: count # データの取得件数
     })
 
-    if response.status == 200
+    if response.status == 200 # これで取得したデータをハッシュ形式に変換
       JSON.parse(response.body)["results"]["shop"]
     else
       []
