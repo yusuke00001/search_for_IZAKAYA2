498bcf5c710f834c6f722949a0f7a4fe6aa63c40
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-02-07 12:38:25 +0900

    API機能と確認ビュー

--- app/services/hotpepper_api.rb
+++ app/services/hotpepper_api.rb
@@ -1,16 +1,21 @@
-require "faraday"
+require "net/http"
 require "json"
+require "uri"
 
 class HotpepperAPI
   BASE_URL = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
 
-  def self.search_shops(keyword, count = 10) # これでAPIにリクエストを送信
-    response = Faraday.get(BASE_URL, {
+  def self.search_shops(keyword, count = 10)
+    uri = URI(BASE_URL)
+    params = {
       key: ENV["HOTPEPPER_API_KEY"],
       keyword: keyword, # 検索に使うキーワード
       format: "json", # 受け取るデータの形式
       count: count # データの取得件数
-    })
+    }
+    uri.query = URI.encode_www_form(params)
+
+    response = Net::HTTP.get_response(uri) # APIにリクエストを送信
 
     if response.status == 200 # これで取得したデータをハッシュ形式に変換
       JSON.parse(response.body)["results"]["shop"]
