507c6b739e3fcb9d6f4aef4148a5721011e75b47
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-02-07 16:27:19 +0900

    ページネーションの導入

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,7 @@
 class ShopsController < ApplicationController
   def index
     keyword = params[:keyword] || "名古屋市"
-    @shops = HotpepperAPI.search_shops(keyword)
+    @shops = HotpepperApi.search_shops(keyword)
+    @shops = Kaminari.paginate_array(@shops).page(params[:page]).per(Shop.page_number)
   end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,8 @@ class Shop < ApplicationRecord
   has_many :bookmarks
 
   validates :name_of_shop, presence: true
+
+  def self.page_number
+    10
+  end
 end
