ef94b0cdf54f4372ba1cf1a00e84a5b8f6db3b1a
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-02-19 12:06:39 +0900

    APIのすべてのデータを取得できるように修正

--- app/models/shop_keyword.rb
+++ app/models/shop_keyword.rb
@@ -1,4 +1,6 @@
 class ShopKeyword < ApplicationRecord
   belongs_to :shop
   belongs_to :keyword
+
+  validates :shop_id, uniqueness: { scope: :keyword_id } # shop_idとkeyword_idの組み合わせの重複拒否
 end
--- app/services/hotpepper_api.rb
+++ app/services/hotpepper_api.rb
@@ -5,12 +5,14 @@ require "uri"
 class HotpepperApi
   BASE_URL = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
 
-  def self.search_shops(keyword:)
+  def self.search_shops(keyword:, start:)
+    binding.pry
     uri = URI(BASE_URL)
     params = {
       key: ENV["HOTPEPPER_API_KEY"],
       keyword:, # 検索に使うキーワード
       format: "json", # 受け取るデータの形式
+      start: start,
       count: count = 100
     }
     uri.query = URI.encode_www_form(params)
