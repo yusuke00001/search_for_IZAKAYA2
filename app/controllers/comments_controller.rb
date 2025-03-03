4dd31a4a3a8208c16f0700431619c5da7fa6d733
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-03-03 16:29:03 +0900

    コメントに来店日の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :visit_day)
   end
 end
